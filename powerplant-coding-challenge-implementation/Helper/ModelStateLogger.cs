0134520c7ffbbe875a9a3eac7fcea1e369a06fe4
francois_le_roy <francois.leroy@alten.be>
2023-05-23 19:06:21 +0200

    Add logger for code 401

---
+++ powerplant-coding-challenge-implementation/Helper/ModelStateLogger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace powerplant_coding_challenge_implementation.Logging
+{
+    public static class ModelStateLogger
+    {
+        public static void LogErrors(ModelStateDictionary modelState, ILogger logger)
+        {
+            IEnumerable<string> errorMessages = modelState.Values
+                .SelectMany(v => v.Errors)
+                .Select(e => e.ErrorMessage);
+
+            foreach (string errorMessage in errorMessages)
+            {
+                logger.LogWarning(errorMessage);
+            }
+        }
+    }
+}
